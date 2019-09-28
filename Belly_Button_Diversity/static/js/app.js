8b2a2b50f24f09bd24bcaa3420d234144e4d54e9
Ivonne <ivvyami@outlook.com>
2019-09-28 16:18:41 -0500

    looping through and appending metadata

--- Belly_Button_Diversity/static/js/app.js
+++ Belly_Button_Diversity/static/js/app.js
@@ -3,17 +3,29 @@ function buildMetadata(sample) {
   // @TODO: Complete the following function that builds the metadata panel
 
   // Use `d3.json` to fetch the metadata for a sample
-    // Use d3 to select the panel with id of `#sample-metadata`
+    var url = "/api/belly_button";
+    d3.json(url).then(function(response) {
 
+      console.log(response);
+
+    var data = response;      
+    // Use d3 to select the panel with id of `#sample-metadata`
     // Use `.html("") to clear any existing metadata
+    d3.select("#sample-metadata").html("");
 
     // Use `Object.entries` to add each key and value pair to the panel
+    Object.entries(data).forEach(([key, value]) => {
+      let metaData = d3.select("sample-metadata");
+      metaData.append("h4")
+      .text(`${key}: ${value}`);
+  });
+
     // Hint: Inside the loop, you will need to use d3 to append new
     // tags for each key-value in the metadata.
-
+    let d3.select("#sample-metadata")
     // BONUS: Build the Gauge Chart
     // buildGauge(data.WFREQ);
-}
+};
 
 function buildCharts(sample) {
 
